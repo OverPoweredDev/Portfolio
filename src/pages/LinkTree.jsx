67f9ae666c692336f90a55c42760737bd6e23b13
Omkar Prabhune <mail@omkarprabhune.com>
2025-08-17 09:15:37 +0530

    change canonical URL to remove the www. Also update linktree to have a larger margin

--- src/pages/LinkTree.jsx
+++ src/pages/LinkTree.jsx
@@ -2,7 +2,7 @@ import Connect from "../components/Connect";
 
 export default function LinkTree() {
   return (
-    <div className="w-screen">
+    <div className="w-screen mt-20">
       <Connect />
     </div>
   );
